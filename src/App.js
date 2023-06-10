26af1062b09210990ac5e0e3be53e9e8e7bb331e
Paulo <pmarquesbaronefilho18@protonmail.com>
2023-06-10 13:57:10 -0300

    add message ballon

--- src/App.js
+++ src/App.js
@@ -1,65 +1,88 @@
 import runner from './public/img/runner.gif';
+import stay from './public/img/stay.png';
 import floor from './public/img/floor.png';
+import ballon from './public/img/ballon.png';
 import ball from './public/img/ball.gif';
 import dead from './public/img/dead.gif';
 import clouds1 from './public/img/clouds/1.png';
 import clouds2 from './public/img/clouds/2.png';
 import clouds3 from './public/img/clouds/3.png';
 import clouds4 from './public/img/clouds/4.png';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const [ game, setGame ] = useState(false);
   
-  const handleJump = (event) => {
-    if(event.code === 'Space' && !document.querySelector('.runner').classList.contains('jump')) {
+  const handleJump = () => {
+    console.log(`handleJump: ${game}`);
+    if(game) {
       const runnerPerson = document.querySelector('.runner');
-  
-      setTimeout(() => {
-        runnerPerson.classList.remove('jump');
-      }, 1000);
-  
-      runnerPerson.classList.add('jump')
+      if(!runnerPerson.classList.contains('jump')) {
+        runnerPerson.classList.add('jump')
+    
+        setTimeout(() => {
+          runnerPerson.classList.remove('jump');
+        }, 1000);
+      }
+    } else {
+      startGame()
     }
   }
 
+  useEffect(() => {
+    console.log(`useEffect: ${game}`)
+  }, [game])
+
   const check = setInterval(() => {
     const ball = document.querySelector('.ball');
-    const runnerPerson = document.querySelector('.runner');
-
-    const runnerPersonBottom = runnerPerson.offsetTop + runnerPerson.offsetHeight;
     const ballTop = ball.offsetTop;
-
-    const runnerPersonRight = runnerPerson.offsetLeft + runnerPerson.offsetWidth;
     const ballLeft = ball.offsetLeft;
 
+    const runnerPerson = document.querySelector('.runner');
+    const runnerPersonBottom = runnerPerson.offsetTop + runnerPerson.offsetHeight;
+    const runnerPersonRight = runnerPerson.offsetLeft + runnerPerson.offsetWidth;
+
     if(runnerPersonBottom >= ballTop && runnerPersonRight >= ballLeft) {
-      const floor = document.querySelector('.floor');
-      const runnerDead = document.querySelector('.runner-dead');
-      const floorPosition = window.getComputedStyle(floor).transform;
-      
-      ball.style.animation = 'none';
-      ball.style.display = 'none';
-
-      runnerPerson.style.display = 'none';
-      runnerPerson.style.animation = 'none';
-
-      runnerDead.style.display = 'block';
-
-      floor.style.transform = floorPosition;
-      floor.style.animation = 'none';
-
+      gameOver();
       clearInterval(check)
     }
   }, 10);
 
+  document.addEventListener('keyup', handleJump)
 
-  document.addEventListener('keypress', handleJump);
+  const gameOver = () => {
+    setGame(false);
+
+    const runnerPerson = document.querySelector('.runner');
+    const ball = document.querySelector('.ball');
+    const floor = document.querySelector('.floor');
+    const floorPosition = window.getComputedStyle(floor).transform;
+
+    runnerPerson.src = dead;
+    runnerPerson.classList.remove('jump');
+    floor.classList.remove('floor-animation');
+    floor.style.transform = floorPosition;
+    ball.classList.remove('ball-animation');
+  }
+
+  const startGame = () => {
+    const runnerPerson = document.querySelector('.runner');
+    const ball = document.querySelector('.ball');
+    const floor = document.querySelector('.floor');
+    
+    runnerPerson.src = runner;
+    ball.classList.add('ball-animation');
+    floor.classList.add('floor-animation');
+
+    return setGame(true);
+  }
 
   return (
     <section className="game-container">
       <div className="sky"></div>
       <img src={floor} alt="floor" className='floor' />
-      <img className='runner' src={runner} alt="Runner" />
-      <img className='runner-dead' src={dead} alt="Runner Dead" />
+      <img className='runner' src={stay} alt="Runner" />
+      {!game && <img className='ballon' src={ballon} alt="Balão de conversa" />}
       <img className='ball' src={ball} alt="Ball" />
       <img className='clouds clouds1' src={clouds1} alt="clouds" />
       <img className='clouds clouds2' src={clouds2} alt="clouds" />
